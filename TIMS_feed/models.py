924af4e946b00b5995c8a316c75b36c017a19956
Acton <actonlebrein@gmail.com>
2018-03-26 23:05:36 -0400

    Database
    Created database to make it more efficient

--- TIMS_feed/models.py
+++ TIMS_feed/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Disruptions(models.Model):
+	id=models.IntegerField(primary_key=True)
+	street_name=models.CharField(max_length=200)
+	longitude=models.FloatField(null=False)
+	latitude=models.FloatField(null=False)
--- TIMS_feed/views.py
+++ TIMS_feed/views.py
@@ -3,29 +3,32 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.conf import settings
+from TIMS_feed.models import Disruptions
 import urllib2
 import xmltodict
 import json
-
 
 def get_disruptions(request):
 	context = {}
 	disruption_data=[]
 	template = 'TIMS_feed/map.html'
-	if len(disruption_data)==0:
-		XML_file = urllib2.urlopen('https://data.tfl.gov.uk/tfl/syndication/feeds/tims_feed.xml?app_id=&app_key=')
-		data = XML_file.read()
-		XML_file.close()
+	XML_file = urllib2.urlopen('https://data.tfl.gov.uk/tfl/syndication/feeds/tims_feed.xml?app_id=&app_key=')
+	data = XML_file.read()
+	XML_file.close()
+	data = xmltodict.parse(data)
 
-		data = xmltodict.parse(data)
-		for d in data['Root']['Disruptions']['Disruption']:
-			lat_lon=d['CauseArea']['DisplayPoint']['Point']['coordinatesLL'].split(',')
-			location=' '+d['location'].replace("'","")
-			disruption_id='disruption id '+d['@id']
-			print 'Disruption id is {0}'.format(disruption_id)
-			print 'Location is {0}'.format(location)
-			print 'Coordinates are {0}'.format(lat_lon)
-			disruption_data.append([float(lat_lon[0]),float(lat_lon[1]),disruption_id+location])
+	for d in data['Root']['Disruptions']['Disruption']:
+		id=int(d['@id'])
+		lat_lon=d['CauseArea']['DisplayPoint']['Point']['coordinatesLL'].split(',')
+		location=d['location'].replace("'","")
+		res=Disruptions.objects.filter(id=id)
+		if len(res)==0:
+			disruption_data.append([float(lat_lon[0]),float(lat_lon[1]),'disruption id '+str(id)+' '+location])
+			dis=Disruptions.objects.create(id=id,street_name=location,longitude=float(lat_lon[0]),latitude=float(lat_lon[1]))
+		else:
+			res=res[0]
+			disruption_data.append([res.longitude,res.latitude,'disruption id '+str(res.id)+' '+res.street_name])
+
 	disruption_data=json.dumps(disruption_data)
 	context['disruption_data']=disruption_data
 	return render(request,template,context)
